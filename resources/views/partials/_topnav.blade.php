bfc4974a88d74e5295efa9c662490c9c136deafb
Khan Mohammad Rashedun-Naby <Khan Mohammad Rashedun-Naby>
2016-03-03 15:09:26 +0600

    User And Clents View Clean Up.

--- resources/views/partials/_topnav.blade.php
+++ resources/views/partials/_topnav.blade.php
@@ -13,16 +13,8 @@
                         <i class="fa fa-bars"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
-                        <li>
-                            <a href="{{URL::to('users')}}"><i class="fa fa-users pull-right"></i> Users</a>
-                        </li>
-                        <li>
-                            <a href="{{URL::to('users/create')}}">
-                                <span><i class="fa fa-user pull-right"></i> Add New User</span>
-                            </a>
-                        </li>
-                        <li><a href="{{URL::to('logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
-                        </li>
+                        <li><a href="{{URL::to('users')}}"><i class="fa fa-users pull-right"></i> Users</a></li>
+                        <li><a href="{{URL::to('logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                     </ul>
                 </li>
             </ul>
