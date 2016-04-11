e8489591db6f8408acfe238e90ad5316ae3b17aa
Khan Mohammad Rashedun-Naby <Khan Mohammad Rashedun-Naby>
2016-04-12 01:52:37 +0600

    Some JavaScript Clean Up.

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,6 +4,109 @@
  * and open the template in the editor.
  */
 
+
+/** ******  Inline JS Codes  *********************** **/
+$(document).ready(function () {
+    $('select').select2();
+    $('input.tableflat').iCheck({
+        checkboxClass: 'icheckbox_flat-green',
+        radioClass: 'iradio_flat-green'
+    });
+});
+
+var asInitVals = new Array();
+$(document).ready(function () {
+    $("tfoot input").keyup(function () {
+        /* Filter on the column based on the index of this element's parent <th> */
+        oTable.fnFilter(this.value, $("tfoot th").index($(this).parent()));
+    });
+    $("tfoot input").each(function (i) {
+        asInitVals[i] = this.value;
+    });
+    $("tfoot input").focus(function () {
+        if (this.className == "search_init") {
+            this.className = "";
+            this.value = "";
+        }
+    });
+    $("tfoot input").blur(function (i) {
+        if (this.value == "") {
+            this.className = "search_init";
+            this.value = asInitVals[$("tfoot input").index(this)];
+        }
+    });
+});
+
+// initialize the validator function
+validator.message['date'] = 'not a real date';
+
+// validate a field on "blur" event, a 'select' on 'change' event & a '.reuired' classed multifield on 'keyup':
+$('.form-label-left')
+    .on('blur', 'input[required], input.optional, select.required', validator.checkField)
+    .on('change', 'select.required', validator.checkField)
+    .on('keypress', 'input[required][pattern]', validator.keypress);
+
+$('.multi.required')
+    .on('keyup blur', 'input', function () {
+        validator.checkField.apply($(this).siblings().last()[0]);
+    });
+
+// bind the validation to the form submit event
+// $('#send').click('submit');
+// .prop('disabled', true);
+
+$('.form-label-left').submit(function (e) {
+    e.preventDefault();
+    var submit = true;
+    // evaluate the form using generic validaing
+    if (!validator.checkAll($(this))) {
+        submit = false;
+    }
+
+    if (submit)
+        this.submit();
+    return false;
+});
+
+/* FOR DEMO ONLY */
+$('#vfields').change(function () {
+    $('form').toggleClass('mode2');
+}).prop('checked', false);
+
+$('#alerts').change(function () {
+    validator.defaults.alerts = (this.checked) ? false : true;
+    if (this.checked)
+        $('form .alert').remove();
+}).prop('checked', false);
+
+$('button.delete').on('click', function() {
+    swal({
+        title: "Are you sure?",
+        text: "Think again. This data can't be recovered later !",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        closeOnConfirm: false,
+        closeOnCancel: false
+    }, function(isConfirm) {
+        if (isConfirm) {
+            $("form#delete").submit();
+        } else {
+            // swal("Cancelled", "Your data is safe.", "error");
+            swal({
+                title:  "Delete Cancelled",   
+                text:   "Your data is safe.", 
+                type:   "error",  
+                timer:  1000,   
+                showConfirmButton: false
+            });
+        }
+    });
+})
+
+
 /** ******  left menu  *********************** **/
 $(function () {
     $('#sidebar-menu li ul').slideUp();
