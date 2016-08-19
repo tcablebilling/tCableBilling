d8d6591c3622eaf724eaefec186ec3614e5a6004
Khan Mohammad Rashedun-Naby <naby88@gmail.com>
2016-08-19 15:13:39 +0000

    Gulp Initiated

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+    mix.scripts([
+		'/../../../node_modules/jquery/dist/jquery.min.js',
+		'/../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+		'/../../../node_modules/sweetalert/dist/sweetalert.min.js',
+		'/../../../node_modules/jquery-ui-bundle/jquery-ui.min.js'
+    ], 'public/js/top.js');
+    mix.scripts([
+		'/../../../node_modules/jquery.nicescroll/jquery.nicescroll.min.js',
+		'/../../../node_modules/icheck/icheck.min.js',
+		'/../../../node_modules/datatables/media/js/jquery.dataTables.min.js',
+		'/../../../node_modules/datatables-tabletools/js/dataTables.tableTools.js',
+		'/../../../node_modules/validator/validator.js',
+		'/../../../node_modules/moment/min/moment.min.js',
+		'/../../../node_modules/daterangepicker/daterangepicker.min.js',
+		'/../../../node_modules/select2/dist/js/select2.min.js',
+		'/../../../node_modules/accounting/accounting.min.js',
+		// '/../../../node_modules/vue-sortable',
+		'/../../../node_modules/vue/dist/vue.js',
+		'custom.js',
+    ], 'public/js/bottom.js');
+});
