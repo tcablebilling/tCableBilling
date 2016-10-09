2d956afd5125f09b988e66ef03ac7682cd1871a1
Khan Mohammad Rashedun-Naby <naby88@gmail.com>
2016-10-09 18:43:47 +0600

    Monthly Bill Slip Added To Bill PDF

--- resources/views/invoices/monthly.blade.php
+++ resources/views/invoices/monthly.blade.php
@@ -119,10 +119,56 @@
 			table td.unit {
 				font-size:1.2em;
 			}
+
+			.sign-area {
+				position: relative;
+				display: block;
+				padding: 105px 0px 85px 0px;
+			}
+
+			.collector-sign {
+				position:absolute;
+				left:0;
+			}
+
+			.collector-sign span {
+				display: block;
+			}
+
+			.client-sign {
+				position:absolute;
+				right:0
+			}
+
+			.client-sign span {
+				display: block;
+				text-align: right;
+			}
+
 			#notices .notice {
 				color:#5D6975;
 				font-size:1.2em;
 			}
+
+			div#copyright {
+				width: 100%;
+				height: 30px;
+				border-top: 1px solid #C1CED9;
+				padding: 8px 0;
+				text-align: center;
+				position: absolute;
+				top: 600px;
+			}
+
+			.slip {
+				position: absolute;
+				bottom: 250px;
+			}
+
+			.slip .sign-area {
+				bottom: -80px;
+			}
+
 			footer {
 				width:100%;
 				height:30px;
@@ -185,10 +231,69 @@
 			            </tr>
 			        </tbody>
 			    </table>
+				<div class="sign-area">
+					<div class="collector-sign">
+						<span>
+							================================
+						</span>
+						<span>
+							Collector Sign
+						</span>
+					</div>
+					<div class="client-sign">
+						<span>
+							================================
+						</span>
+						<span>
+							Client Sign
+						</span>
+					</div>
+				</div>
 			    <div id="notices">
 			        <div>NOTICE:</div>
 			        <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
 			    </div>
+				<div id="copyright">
+					Copyright &copy; {{date('Y')}} All Rights Reserved. tCableBilling, an automated billing system.
+				</div>
+				<div class="slip">
+					<span>
+						================================================================================================
+					</span>
+					<h1>BILL ID:{{sprintf("%'.05d\n", $billing->id)}}</h1>
+					<div id="company" class="clearfix">
+						<div>Company Name</div>
+						<div>455 Foggy Heights,<br/> AZ 85004, US</div>
+						<div>(602) 519-0450</div>
+						<div><a href="mailto:company@example.com">company@example.com</a></div>
+					</div>
+					<div id="project">
+						<div><span>ID</span> {{$billing->clientDetails->area_name->code . '-' . sprintf("%'.03d\n", $billing->clientDetails->id)}}</div>
+						<div><span>CLIENT</span> {{$billing->clientDetails->name}}</div>
+						<div><span>ADDRESS</span> {{$billing->clientDetails->address}}</div>
+						<div><span>PHONE</span> {{$billing->clientDetails->phone_no_1}}</div>
+						<div><span>DATE</span> {{date('F j, Y')}}</div>
+						<div><span>BILL FOR</span> {{date('F Y', strtotime($billing->month))}}</div>
+					</div>
+					<div class="sign-area">
+						<div class="collector-sign">
+						<span>
+							================================
+						</span>
+							<span>
+							Collector Sign
+						</span>
+						</div>
+						<div class="client-sign">
+						<span>
+							================================
+						</span>
+							<span>
+							Client Sign
+						</span>
+						</div>
+					</div>
+				</div>
 			</main>
 			<footer>Copyright &copy; {{date('Y')}} All Rights Reserved. tCableBilling, an automated billing system.</footer>
 			@if( $i != count($billings))
