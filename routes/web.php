14af2b4a72793b2604e6f833d641f60efa005ca6
Khan M Rashedun-Naby <naby88@gmail.com>
2017-10-16 20:29:47 +0600

    Route Cleaning

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,59 @@
 |
 */
 
-Route::group(['middleware' => 'auth'], function(){
-	Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index'] );
-	Route::get('/print-m', ['as' => 'print-m', 'uses' => 'HomeController@billMonthly'] );
-	Route::get('/print-custom', ['as' => 'print-custom', 'uses' => 'HomeController@clientCustom'] );
-	
-	Route::get('/individual', [ 'as' => 'individual', 'uses' => 'BillingsController@individualClient'] );
-	
-	Route::get('/db-backup', ['as' => 'db-backup', 'uses' => 'HomeController@dbBackup'] );
-	
-	Route::resource( '/users', 'UsersController' );
-	Route::resource( '/clients', 'ClientsController' );
-	Route::resource( '/packages', 'PackagesController' );
-	Route::resource( '/areas', 'AreasController' );
-	Route::resource( '/payments', 'PaymentsController');
-	Route::resource( '/billings', 'BillingsController');
-	Route::resource( '/employees', 'EmployeesController');
-});
+Route::group(
+	[ 'middleware' => 'auth' ],
+	function () {
+		Route::get( '/home', [ 'as' => 'home', 'uses' => 'HomeController@index' ] );
+		Route::get(
+			'/print-m',
+			[ 'as' => 'print-m', 'uses' => 'HomeController@billMonthly' ]
+		);
+		Route::get(
+			'/print-custom',
+			[
+				'as'   => 'print-custom',
+				'uses' => 'HomeController@clientCustom',
+			]
+		);
+
+		Route::get(
+			'/individual', [
+			'as'   => 'individual',
+			'uses' => 'BillingsController@individualClient',
+		]
+		);
+
+		Route::get(
+			'/db-backup',
+			[
+				'as' => 'db-backup',
+				'uses' => 'HomeController@dbBackup',
+			]
+		);
+
+		Route::resource( '/users',      'UsersController' );
+		Route::resource( '/clients',    'ClientsController' );
+		Route::resource( '/packages',   'PackagesController' );
+		Route::resource( '/areas',      'AreasController' );
+		Route::resource( '/payments',   'PaymentsController' );
+		Route::resource( '/billings',   'BillingsController' );
+		Route::resource( '/employees',  'EmployeesController' );
+	}
+);
+
 // Authentication routes...
 Route::get( '/', 'HomeController@rootPath' );
-Route::get( '/login', [ 'as' => 'getLogin', 'uses' => 'LoginController@showLoginForm' ] );
-Route::post( '/login', [ 'as' => 'postLogin', 'uses' => 'LoginController@login' ] );
+
+Route::get(
+	'/login',
+	[
+		'as' => 'getLogin',
+		'uses' => 'LoginController@showLoginForm',
+	]
+);
+
+Route::post(
+	'/login', [ 'as' => 'postLogin', 'uses' => 'LoginController@login' ]
+);
 Route::get( 'logout', 'LoginController@logout' );
