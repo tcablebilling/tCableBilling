05ab37e413f9df9e0b3a9275dc46eb666d68b34b
Khan M Rashedun-Naby <naby88@gmail.com>
2018-01-13 20:29:47 +0600

    BillingsController Refactoring - YODA Implementation

--- app/Http/Controllers/BillingsController.php
+++ app/Http/Controllers/BillingsController.php
@@ -3,9 +3,8 @@
 namespace TCableBilling\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use TCableBilling\Http\Models\Client;
 use TCableBilling\Http\Models\Billing;
+use TCableBilling\Http\Models\Client;
 
 /**
  * Class BillingsController
@@ -69,53 +68,59 @@ class BillingsController extends Controller {
 	/**
 	 * Store a newly created resource in storage.
 	 *
-	 * @param  \Illuminate\Http\Request $request
+	 * @param \TCableBilling\Http\Models\Billing $billing
 	 *
-	 * @return \Illuminate\Http\Response
+	 * @return mixed
 	 */
 	public function store( Billing $billing ) {
-		$clients = Client::where('client_status', '=', 'Active')->get();
-		$data = array();
-		$created_at =   \DB::table( 'billings' )
-						->orderBy( 'created_at', 'desc' )
-						->first();
-		$date = 0;
+
+		$clients    = Client::where( 'client_status', '=', 'Active' )
+		                    ->get();
+		$created_at = \DB::table( 'billings' )
+		                 ->orderBy( 'created_at', 'desc' )
+		                 ->first();
+		$date       = 0;
+
 		if ( $created_at ) {
 			$date = date( 'Ym', strtotime( $created_at->created_at ) );
 		}
-		if ( date( 'Ym' ) != $date ) {
+
+		if ( date( 'Ym' ) !== $date ) {
 			foreach ( $clients as $client ) {
-				$package =    \DB::table( 'packages' )
+				$package = \DB::table( 'packages' )
 				              ->where( 'id', '=', $client->package_id )
 				              ->get();
-				$data = array(
+				$data    = [
 					'client_id'   => $client->id,
-					'bill_amount' => $package[0]->fee,
+					'bill_amount' => $package[ 0 ]->fee,
 					'month'       => date(
 						'Ymd',
 						strtotime(
 							"+1 month",
-							strtotime( date( 'Ym' ).'01' )
+							strtotime( date( 'Ym' ) . '01' )
 						)
-					)
-				);
+					),
+				];
 				$billing->create( $data );
 			}
 		}
 
-		if ( env('DB_CONNECTION') == 'sqlite' ) {
-			$database = storage_path('database.sqlite');
+		if ( env( 'DB_CONNECTION' ) == 'sqlite' ) {
+			$database        = storage_path( 'database.sqlite' );
 			$backup_database = env( 'DB_BACKUP_DIR', storage_path() )
 			                   . date( 'Y-m-d-H-i-s' )
 			                   . '_datatbase.sqlite';
-			copy($database, $backup_database);
+			copy( $database, $backup_database );
 		}
 
-        \Alert::success(
-        	'Monthly bill for all clients has been generated.<br/>
+		\Alert::success(
+			'Monthly bill for all clients has been generated.<br/>
 			Monthly database backup also completed.',
-	        'Bill & Backup Done!'
-        )->html('true')->persistent('Close');
+			'Bill & Backup Done!'
+		)
+		      ->html( 'true' )
+		      ->persistent( 'Close' );
+
 		return \Redirect::to( 'home' );
 	}
 
@@ -127,6 +132,7 @@ class BillingsController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function show( $id ) {
+
 		return \Redirect::to( 'billings' );
 	}
 
@@ -138,6 +144,7 @@ class BillingsController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function edit( $id ) {
+
 		return \Redirect::to( 'billings' );
 	}
 
@@ -145,11 +152,12 @@ class BillingsController extends Controller {
 	 * Update the specified resource in storage.
 	 *
 	 * @param  \Illuminate\Http\Request $request
-	 * @param  int $id
+	 * @param  int                      $id
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
 	public function update( Request $request, $id ) {
+
 		return \Redirect::to( 'billings' );
 	}
 
@@ -161,52 +169,55 @@ class BillingsController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function destroy( $id ) {
-        $billing = Billing::findOrFail($id);
-        $billing->delete();
-        \Alert::info(
-        	'Your requested bill has been deleted.',
-	        'Bill Deleted !'
-        );
-        return \Redirect::to('/billings');
+
+		$billing = Billing::findOrFail( $id );
+		$billing->delete();
+		\Alert::info(
+			'Your requested bill has been deleted.',
+			'Bill Deleted !'
+		);
+
+		return \Redirect::to( '/billings' );
 	}
-	
+
 	/**
 	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
-	public function individualClient()
-	{
-		$input = explode('-', \Input::get( 'range' ), 2);
+	public function individualClient() {
+
+		$input    = explode( '-', \Input::get( 'range' ), 2 );
 		$input_fm = null;
 		$input_tm = null;
-		if (!empty($input)) {
-			$input_fm = $input[0];
-			if (array_key_exists( 1, $input)) {
-				$input_tm = $input[1];
+		if ( ! empty( $input ) ) {
+			$input_fm = $input[ 0 ];
+			if ( array_key_exists( 1, $input ) ) {
+				$input_tm = $input[ 1 ];
 			}
 		}
-		$from_month = date('Ym', strtotime( $input_fm ));
-		$to_month = date(
+		$from_month = date( 'Ym', strtotime( $input_fm ) );
+		$to_month   = date(
 			'Ymd',
 			strtotime(
 				date( 'Y-m-d', strtotime( $input_tm ) )
 			)
 		);
-		$client_id = null;
-		$clients   = [ ];
+		$client_id  = null;
+		$clients    = [];
 		foreach ( Client::all() as $client ) {
 			$clients[ $client->id ] = $client->area_name
-				                            ->code . '-' . sprintf(
-											    "%'.03d\n",
-											    $client->id
-											) . ' ' . $client->name;
+				                          ->code . '-' . sprintf(
+				                          "%'.03d\n",
+				                          $client->id
+			                          ) . ' ' . $client->name;
 		}
 		$client_id = \Input::get( 'client_id' );
-		$billings = Billing::with('billCumulative', 'paidCumulative');
-		$billings = $billings
+		$billings  = Billing::with( 'billCumulative', 'paidCumulative' );
+		$billings  = $billings
 			->where( 'client_id', $client_id )
 			->orderBy( 'id', 'DESC' )
-			->whereBetween('month', array( $from_month, $to_month))
+			->whereBetween( 'month', [ $from_month, $to_month ] )
 			->paginate( 150 );
+
 		return view(
 			'individual_bill',
 			compact(
